178b60a6263e4d51a2ff56e03c3ef85f3b5072fe
jcgarzont <camilo_tellez04@hotmail.com>
2019-10-28 23:54:43 -0500

    Se creó la prueba unitaria para el metodo invertirCadena

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/AppTest.java
@@ -1,9 +1,47 @@
 package com.hbt.semillero.servicios;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class AppTest {
 
 	public void shouldAnswerWithTrue() {
 
+	}
+	
+	public void primeraPU() {
+		Long resultadoEsperado = 150L;
+		Long sumando1 = 100L; 
+		Long sumando2 = 50L;
+		Long resultado = sumando1 + sumando2;
+		Assert.assertEquals(resultado, resultadoEsperado);
+		resultadoEsperado = 200L;
+		Assert.assertNotEquals(resultado, resultadoEsperado);	
+
+	}
+	@Test
+	public void inverticCadenaPU() {
+		
+		String cadena = "Juan Camilo";
+		//System.out.println(cadena);
+		String cadenaInvertida = invertirCadena(cadena);
+		//System.out.println(cadenaInvertida);
+		
+		Assert.assertNotNull(cadena);
+		Assert.assertNotNull(cadenaInvertida);
+
+		for (int x = 0; x < cadena.length(); x++) 
+		{
+			Assert.assertEquals(cadena.charAt(x), cadenaInvertida.charAt(cadena.length()-1-x));
+		}
+	}
+	
+	
+	private String invertirCadena(String cadena) {
+		String cadenaInvertida = "";
+		for (int x = cadena.length() - 1; x >= 0; x--) {
+			cadenaInvertida = cadenaInvertida + cadena.charAt(x);
+			}
+		return cadenaInvertida;
 	}
 }
