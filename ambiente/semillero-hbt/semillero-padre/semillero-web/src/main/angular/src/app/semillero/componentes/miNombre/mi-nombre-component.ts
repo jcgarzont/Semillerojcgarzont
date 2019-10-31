f0cbdffab72e8cc2dea101c472e8d473455b0dc2
jcgarzont <camilo_tellez04@hotmail.com>
2019-10-30 23:34:11 -0500

    Creacion de los componentes mi-nombre y crear-persona

--- ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/miNombre/mi-nombre-component.ts
+++ ambiente/semillero-hbt/semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/miNombre/mi-nombre-component.ts
@@ -13,5 +13,5 @@ import { Component } from "@angular/core";
  })
 
  export class MiNombre{
-     nombreConCiudad = "Juan Camilo Garzon Tellez - Armenia";
+     nombreConCiudad :String = "Juan Camilo Garzon Tellez - Armenia";
  }
