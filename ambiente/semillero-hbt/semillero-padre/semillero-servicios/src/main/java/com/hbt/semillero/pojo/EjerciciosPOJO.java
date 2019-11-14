b94033a4bd29f314eac0c7637a915ed066042606
jcgarzont <camilo_tellez04@hotmail.com>
2019-11-13 19:43:18 -0500

    ejercicio 4

--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/pojo/EjerciciosPOJO.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/pojo/EjerciciosPOJO.java
@@ -3,6 +3,8 @@
  */
 package com.hbt.semillero.pojo;
 
+import java.time.LocalDate;
+
 /**
  * <b>Descripción:<b> Clase que determina
  * <b>Caso de Uso:<b> 
@@ -11,4 +13,32 @@ package com.hbt.semillero.pojo;
  */
 public class EjerciciosPOJO {
 
+	public boolean primos(int numero) {
+		int contador = 2;
+		boolean primo=true;
+		while ((primo) && (contador!=numero)){
+			if (numero % contador == 0)
+			{
+				primo = false;
+			    contador++;
+			}
+			else
+			{
+				primo = true;
+			}
+		}
+		return primo;
+	}
+	
+	public boolean fecha(int nacimiento, int numeroAños) {
+		LocalDate fechaActual = LocalDate.now();
+		int añoActual = fechaActual.getYear();
+		int diferecia = añoActual - numeroAños ;
+		if (diferecia == nacimiento) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
--- ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/EjerciciosTest.java
+++ ambiente/semillero-hbt/semillero-padre/semillero-servicios/src/test/java/com/hbt/semillero/servicios/EjerciciosTest.java
@@ -5,6 +5,7 @@ package com.hbt.semillero.servicios;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import com.hbt.semillero.pojo.EjerciciosPOJO;;
 
 /**
  * <b>Descripción:<b> Clase que determina
@@ -21,7 +22,7 @@ public class EjerciciosTest {
 	 * @author Juan Camilo
 	 *
 	 */
-	@Test (enabled=false)
+	//@Test (enabled=false)
 	public void ejercicio1() {
 		// Valido
 		String A$B = "valido?";
@@ -42,13 +43,46 @@ public class EjerciciosTest {
 		//String 1980_s = "valido?";
 		//Assert.assertNotNull(1980_s);	
 	}
-	private String brand;
-	private Boolean empty;
+	private static String brand;
+	private static Boolean empty;
+	
 	public static void ejercicio2() {
 		//WaterBottle wb = new WaterBottle();
 		System.out.print("Empty =" + empty);
 		System.out.print(", Brand =" + brand);
-		
+		// Se espera que contengan Null al no estar inicializados
+		Assert.assertNull(empty);
+		Assert.assertNull(brand);
 	}
-
+	
+	//@Test
+	public void ejercicio3() {
+		EjerciciosPOJO ejerciciosPOJO = new EjerciciosPOJO();
+		boolean test;
+		test = ejerciciosPOJO.primos(5);
+		//Assert.assertTrue(test);
+		test = ejerciciosPOJO.primos(222);
+		//Assert.assertTrue(test);
+		test = ejerciciosPOJO.primos(0);
+		//Assert.assertTrue(test);		
+	}
+	
+	/**
+	 * 
+	 * Metodo encargado probar el ejercicio 4
+	 * <b>Caso de Uso</b>
+	 * @author Juan Camilo
+	 *
+	 */
+	@Test
+	public void ejercicio4() {
+		EjerciciosPOJO ejerciciosPOJO = new EjerciciosPOJO();
+		boolean test;
+		test = ejerciciosPOJO.fecha(1998, 21);
+		//Verifica que el resultado sea true
+		Assert.assertTrue(test);
+		test = ejerciciosPOJO.fecha(1998, 99);
+		//Verifica que el resultado sea false
+		Assert.assertFalse(test);
+	}
 }
